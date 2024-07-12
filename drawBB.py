7f88a8571d8f0e14c5be182da1e57c778dc2eba6
marion <umoya@Pelagornis>
2024-07-12 12:55:23 +0200

    Feat : application working in real time video

--- drawBB.py
+++ drawBB.py
@@ -2,28 +2,12 @@ import cv2
 import pandas as pd
 
 def filter_predictions(df):
-    return df[df['confidence'] >= 5]
+    return df[(df['confidence'] >= 0.5) & (df['class'] == 0)]
 
 
-def extract_detected_object_infos(row):
-   index, data = row
-   
-   detected_object = {
-        'xmin': int(data['xmin']),
-        'ymin': int(data['ymin']),
-        'xmax': int(data['xmax']),
-        'ymax': int(data['ymax']),
-        'confidence': data['confidence'],
-        'label': data['name']
-    }
-   
-   return detected_object
-
-
-def draw_bouding_box_label_and_confidence(data, image):
-
-    # Draw bouding_box
-    cv2.rectangle(image, (data['xmin'], data['ymin']), (data['xmax'], data['ymax']), (0, 0, 255), 2)
+def draw_bounding_box_label_and_confidence(data, image):
+    # Draw bounding box
+    cv2.rectangle(image, (int(data.xmin), int(data.ymin)), (int(data.xmax), int(data.ymax)), (0, 0, 255), 2)
+    
     # Draw label and confidence 
-    cv2.putText(image, f"{data['label']} - {data['confidence']:.2f}", (data['xmin'], data['ymin'] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
+    cv2.putText(image, f"{data.name} - {data.confidence:.2f}", (int(data.xmin), int(data.ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import cv2
 
-from drawBB import filter_predictions, extract_detected_object_infos, draw_bouding_box_label_and_confidence
+from drawBB import filter_predictions, draw_bounding_box_label_and_confidence
 
 # Model Integration
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")
@@ -12,32 +12,59 @@ model = torch.hub.load("ultralytics/yolov5", "yolov5s")
 vidcap = cv2.VideoCapture("http://192.168.1.19:8000/") # Create a variable that will hold the video capturing object.
 
 if vidcap.isOpened():
-    print("Camera is open")
-   
-    ret, frame = vidcap.read()
+    print("Cam open")
 
-    # Inference
-    preds = model(frame)
-    detected_objects = preds.pandas().xyxy[0]
-    # filtered_objects = filter_predictions(detected_objects)
+    while True:
+        ret, frame = vidcap.read()
+        if ret:
 
-    if detected_objects:
-        print(detected_objects)
-        while True:
-            object_to_draw = [extract_detected_object_infos(row) for row in detected_objects.iterrows()]
-            for obj in object_to_draw:
-                draw_bouding_box_label_and_confidence(obj, frame)
+          # Inference
+            preds = model(frame)
+            detected_objects = preds.pandas().xyxy[0]
+            filtered_objects = filter_predictions(detected_objects)
 
-            # Display
+            if len(filtered_objects) > 0:
+                print(filtered_objects)
+
+                for obj in filtered_objects.itertuples():
+                    draw_bounding_box_label_and_confidence(obj, frame) 
+
             cv2.imshow("Frame", frame)
 
             if cv2.waitKey(1) & 0xFF==ord('q'):
                 break
-    else:
-        print("No predictions was made")
-    
-    print("Closing camera")
-    vidcap.release()
 
-else:
-   print("Cannot open camera")
+
+
+# if vidcap.isOpened():
+#     print("Camera is open")
+    
+#     while True:
+#         ret, frame = vidcap.read()
+#         print(f"ret: {ret}, frame: {frame}")
+
+#         if frame is None:
+#             print(" -- No captured frame : break -- ")
+#         else:
+#             # Inference
+#             preds = model(frame)
+#             detected_objects = preds.pandas().xyxy[0]
+#             filtered_objects = filter_predictions(detected_objects)
+
+#             if len(filtered_objects) > 0:
+#                 print(filtered_objects)
+
+#                 for obj in filtered_objects.itertuples():
+#                     draw_bounding_box_label_and_confidence(obj, frame) 
+
+#             # Display
+#             cv2.imshow("Frame", frame)
+
+#             if cv2.waitKey(1) & 0xFF==ord('q'):
+#                 break
+        
+#             print("Closing camera")
+#             vidcap.release()
+
+# else:
+#    print("Cannot open camera")
